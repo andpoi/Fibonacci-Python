fe1873f4b376d726abff87d8e1d7c1caff0008cf
Andy Poirier <andpoi@gmail.com>
2014-09-08 14:47:26 -0400

    Changed Limit

--- fib.py
+++ fib.py
@@ -3,12 +3,14 @@
 fib1=0
 fib2=1
 fib3=0
-limit=90000000000000000000000000000000000000000000000000000000000000000000000000
+counter=0
+limit=10000
 
-while(fib3<limit):
+while(counter<limit):
 	fib3 = fib1 + fib2
 	print fib3
 	fib1 = fib2
 	fib2 = fib3
+	counter+=1
 	
 print "Done"
